a96964744c474154e6a273adad6486b3182a2991
Github EstheimQwu <Github estheimqwu@outlook.com>
2020-09-05 23:46:06 +0800

    update 2020 09 05

--- Player/Player.gd
+++ Player/Player.gd
@@ -7,7 +7,8 @@ const FRICTION = 500
 
 var velocity = Vector2.ZERO
 onready var animationPlayer = $AnimationPlayer
-
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
 # func _ready():
 #	print("hello~")
 #	animationPlayer = $AnimationPlayer	
@@ -17,21 +18,26 @@ func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
-		if input_vector.x >= 0:
-			animationPlayer.play("RunRight")
+		# if input_vector.x >= 0:
+			# animationPlayer.play("RunRight")
 			# velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
-		else:
-			animationPlayer.play("RunLeft")
+		# else:
+			# animationPlayer.play("RunLeft")
 			# velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
+		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-		if input_vector.x > 0:
-			animationPlayer.play("IdleRight")
-		elif input_vector.x < 0:
-			animationPlayer.play("IdleLeft")
+		# if input_vector.x > 0:
+		# 	animationPlayer.play("IdleRight")
+		# elif input_vector.x < 0:
+		# 	animationPlayer.play("IdleLeft")
 		
 			
 	velocity =  move_and_slide(velocity)
