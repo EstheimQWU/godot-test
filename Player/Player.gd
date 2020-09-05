766ffd40b1368678ab3feb4fe9c70ba5d13f1984
estheimqwu <estheimqwu@outlook.com>
2020-09-05 18:09:54 +0800

    animation editing

--- Player/Player.gd
+++ Player/Player.gd
@@ -6,10 +6,12 @@ const ACCELERATION = 500
 const FRICTION = 500
 
 var velocity = Vector2.ZERO
+onready var animationPlayer = $AnimationPlayer
 
-func _ready():
-	print("hello~")
-	pass # Replace with function body.
+# func _ready():
+#	print("hello~")
+#	animationPlayer = $AnimationPlayer	
+#	pass # Replace with function body.
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -18,9 +20,20 @@ func _physics_process(delta):
 	
 	if input_vector != Vector2.ZERO:
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+		if input_vector.x >= 0:
+			animationPlayer.play("RunRight")
+			# velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+		else:
+			animationPlayer.play("RunLeft")
+			# velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+		if input_vector.x > 0:
+			animationPlayer.play("IdleRight")
+		elif input_vector.x < 0:
+			animationPlayer.play("IdleLeft")
 		
+			
 	velocity =  move_and_slide(velocity)
 	
 
